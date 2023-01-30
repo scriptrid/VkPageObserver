6d21eb9a149dbaaca9d2da46ae0df63e42c747bb
Ivan <81517099+scriptrid@users.noreply.github.com>
2023-01-31 00:50:34 +0300

    Add missing files

---
+++ src/main/java/com/example/vkpageobserver/VkPageObserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.vkpageobserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkPageObserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkPageObserverApplication.class, args);
+    }
+
+}
