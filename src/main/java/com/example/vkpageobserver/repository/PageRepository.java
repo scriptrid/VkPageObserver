4c34425d281428167baaf10059e0c8843ff85fd6
Ivan <81517099+scriptrid@users.noreply.github.com>
2023-02-06 01:35:23 +0300

    Remove redundant methods from PageRepository

--- src/main/java/com/example/vkpageobserver/repository/PageRepository.java
+++ src/main/java/com/example/vkpageobserver/repository/PageRepository.java
@@ -4,13 +4,6 @@ import com.example.vkpageobserver.model.entity.PageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface PageRepository extends JpaRepository<PageEntity, Integer> {
-    @Override
-    Optional<PageEntity> findById(Integer integer);
-
-    @Override
-    boolean existsById(Integer integer);
 }
